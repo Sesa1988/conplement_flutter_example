c4706328272f7e47b1d4a011cb28eee5d0ea4b45
SergejSachs <sergej.sachs@conplement.de>
2022-12-01 13:13:06 +0100

    added button for details redirect

--- example/lib/screens/pokemon/pokemon_overview.dart
+++ example/lib/screens/pokemon/pokemon_overview.dart
@@ -1,3 +1,4 @@
+import 'package:example/screens/pokemon_details/pokemon_details.dart';
 import 'package:flutter/material.dart';
 
 class PokemonOverview extends StatelessWidget {
@@ -9,7 +10,14 @@ class PokemonOverview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Pokemons')),
-      body: const Center(child: Text('hello')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed(PokemonDetails.routeName);
+          },
+          child: const Text('go details'),
+        ),
+      ),
     );
   }
 }
