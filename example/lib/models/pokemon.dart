5d42e0eb9719cf8c8c85608f43f89a16cd3c9d10
SergejSachs <sergej.sachs@gmx.de>
2022-12-03 02:41:43 +0100

    removed immutable from pokemon model for simplicity

--- example/lib/models/pokemon.dart
+++ example/lib/models/pokemon.dart
@@ -1,15 +1,9 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
-@immutable
 class Pokemon extends Equatable {
   final String name;
 
   const Pokemon({required this.name});
-
-  Pokemon copyWith({String? name}) {
-    return Pokemon(name: name ?? this.name);
-  }
 
   static Pokemon fromMap(Map<String, dynamic> data) {
     return Pokemon(name: data['name']);
